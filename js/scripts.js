43644f1de62da4d3f184088cfb363cdc58a99e1c
Renee Mei and Sara Frandsen <sara@>
2017-01-19 11:14:27 -0800

    add scripts to find the value of number on each digit

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
 //backend
 var converter = function(input){
+  var output =[];
+  var length = input.toString().length;
+  var inputArray = input.toString().split("");
+  for (var j=1; j<=length; j++){
+    var digit = input.toString().charAt(j-1);
+    var divider = "1"
+    for (var i = 1; i <=j; i++){
+      divider = divider.concat("0");
+    }
+    divider = parseInt(divider);
+    var numDigit = parseInt(digit)*divider;
+    var romanNum = romanNumeralConverter(numDigit);
+    output = output.concat([romanNum]);
+  }
+  return output.join("")
+}
+
+
+var romanNumeralConverter = function(input){
   var str = digitconverter(input);
   var convertedStr = "";
   for (var i=1;i<=str.length;i++){
     if (i === 4){
-      convertedStr = convertedStr.substring(0,1).concat(concat(input, i))
+      convertedStr = convertedStr.substring(0,1).concat(concat(input, i));
     }else if (i === 5){
-      convertedStr = convertedStr.substring(1)
+      convertedStr = convertedStr.substring(1);
     }else if (i === 9){
-      convertedStr = "IX";
+      convertedStr = convertedStr.substring(3).concat(concat(input, i));
     }else{
-    convertedStr = convertedStr.concat(concat(input, i))
+    convertedStr = convertedStr.concat(concat(input, i));
   }
   }
   return convertedStr;
@@ -77,18 +96,24 @@ var concat = function(input, num){
   if (digit ===1){
     if (num ===4){
       return "V"
+    }else if (num ===9){
+      return "X"
     }else{
       return "I"
     }
   }else if (digit ===2){
     if (num ===4){
       return "L"
+    }else if (num === 9){
+      return "C"
     }else{
       return "X"
     }
   }else if (digit ===3){
     if (num ===4){
       return "D"
+    }else if (num === 9){
+      return "M"
     }else{
       return "C"
     }
